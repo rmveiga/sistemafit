107a940603bb0416d03b0e32e5e7f66fc2890867
Rafael Morán <rm.veiga26@gmail.com>
2020-07-25 15:21:14 -0300

    main/templates/unidades/create.html - Implementada verificação se há menssagens do back-end e apresentá-las
    main/templates/unidades/list.html
    - Implementada verificação se há menssagens do back-end e apresentá-las
    
    main/templates/unidades/update.html
    - Implementada verificação se há menssagens do back-end e apresentá-las
    
    unidade/views.py
    - Implementada verificação se a unidade já existe no momento do cadastro
    - Implementada verificação se a unidade já existe no momento da alteração
    - Implementada menssagem de exclusão bem sucedida ao excluir uma unidade

--- unidade/views.py
+++ unidade/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import Unidade
 from .forms import UnidadeModelForm
@@ -14,10 +15,25 @@ def list_unidades(request):
 
 def create_unidade(request):
     if request.method == 'POST':
-        form = UnidadeModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('unidade:list-unidades')
+        sigla_form = request.POST.get('sigla')
+        unidade_filtro = Unidade.objects.filter(sigla__iexact=sigla_form)
+
+        if unidade_filtro:
+            messages.add_message(
+                request, messages.WARNING,
+                f'A unidade {sigla_form} já está cadastrada'
+            )
+            form = UnidadeModelForm(data=request.POST)
+        else:
+            form = UnidadeModelForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.add_message(
+                    request, messages.SUCCESS,
+                    f'Unidade cadastrada com sucesso'
+                )
+
+                return redirect('unidade:list-unidades')
     else:
         form = UnidadeModelForm()
 
@@ -28,18 +44,31 @@ def create_unidade(request):
     return render(request, 'unidades/create.html', context=context)
 
 def update_unidade(request, unidade_id):
-    unidade = Unidade.objects.get(pk=unidade_id)
+    unidade_db = Unidade.objects.get(pk=unidade_id)
 
     if request.method == 'POST':
-        if request.POST.get('sigla') != unidade.sigla:
-            print('diferente')
+        sigla_form = request.POST.get('sigla')
+        unidade_filtro = Unidade.objects.filter(sigla__iexact=sigla_form)
 
-        form = UnidadeModelForm(data=request.POST, instance=unidade)
-        if form.is_valid():
-            form.save()
-            return redirect('unidade:list-unidades')
+        if sigla_form != unidade_db.sigla and len(unidade_filtro) > 0:
+            messages.add_message(
+                request, messages.WARNING,
+                f'A unidade {sigla_form} já está cadastrada'
+            )
+            form = UnidadeModelForm(data=request.POST)
+        else:
+            form = UnidadeModelForm(data=request.POST, instance=unidade_db)
+
+            if form.is_valid():
+                form.save()
+                messages.add_message(
+                    request, messages.SUCCESS,
+                    f'Unidade alterada com sucesso'
+                )
+
+                return redirect('unidade:list-unidades')
     else:
-        form = UnidadeModelForm(instance=unidade)
+        form = UnidadeModelForm(instance=unidade_db)
 
     context = {
         'form': form
@@ -50,4 +79,9 @@ def update_unidade(request, unidade_id):
 def delete_unidade(request, unidade_id):
     unidade = Unidade.objects.get(pk=unidade_id)
     unidade.delete()
+    messages.add_message(
+        request, messages.SUCCESS,
+        f'Unidade excluída com sucesso'
+    )
+
     return redirect('unidade:list-unidades')

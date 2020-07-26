e9b0868829e58b4acb75a85bf9aed131e527d766
Rafael Morán <rm.veiga26@gmail.com>
2020-07-26 13:51:07 -0300

    unidade/models.py - Definido como zero o valor padrão para o campo de dígitos

--- unidade/models.py
+++ unidade/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Unidade(models.Model):
     sigla = models.CharField(max_length=5, verbose_name='Sigla')
-    digitos = models.IntegerField(verbose_name='Dígitos')
+    digitos = models.IntegerField(verbose_name='Dígitos', default=0)
 
     def __str__(self):
         return self.sigla

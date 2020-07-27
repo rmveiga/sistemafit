7b996529b128cd3a1eb5bfdb8929f19d77b2600a
Rafael Morán <rm.veiga26@gmail.com>
2020-07-26 21:00:31 -0300

    acompanhamento/models.py - Aplicado o valor padrão para o campo dt_medicao como a data atual

--- acompanhamento/models.py
+++ acompanhamento/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 from usuario.models import Usuario
 from metrica.models import Metrica
 
 class Acompanhamento(models.Model):
-    dt_medicao = models.DateField(verbose_name='Data de Medição')
+    dt_medicao = models.DateField(verbose_name='Data de Medição', default=timezone.now().date())
     medida = models.FloatField(verbose_name='Medida')
     metrica = models.ForeignKey(Metrica, on_delete=models.DO_NOTHING)
     usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)

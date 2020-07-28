dcaa7388db5f54a5ffe9adbdc6e380d11939afe6
Rafael Morán <rm.veiga26@gmail.com>
2020-07-27 21:42:55 -0300

    usuario/tests.py - Implementado teste que avalia se o método __str__ do model retorna o atributo nome - Implementado teste que avalia se é possível cadastrar um usuário com nome já existente - Implementado teste que avalia se é possível alterar um usuário para um nome já existente

--- usuario/tests.py
+++ usuario/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib import messages
 
-# Create your tests here.
+from .models import Usuario
+
+class UsuarioTestCase(TestCase):
+    def setUp(self):
+        self.usuario = Usuario.objects.create(nome='Rafael')
+
+    def test_str_retorna_nome(self):
+        self.assertEqual(str(self.usuario), 'Rafael')
+
+    def test_cadastrar_usuario_ja_existente(self):
+        client = Client()
+        response = client.post(
+            '/usuarios/create',
+            {
+                'nome': 'Rafael'
+            }
+        )
+
+        storage = messages.get_messages(response.wsgi_request)
+        for msg in storage:
+            self.assertEqual(str(msg), 'O usuário Rafael já está cadastrado')
+
+    def test_editar_usuario_ja_existente(self):
+        client = Client()
+        response = client.post(
+            '/usuarios/update/1',
+            {
+                'nome': 'Rafael'
+            }
+        )
+
+        storage = messages.get_messages(response.wsgi_request)
+        for msg in storage:
+            self.assertEqual(str(msg), 'O usuário Rafael já está cadastrado')
